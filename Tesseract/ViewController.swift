0a4739b88e43d853523031a6cd49f2da18adcad5
Andre Guerra <agu3rra@gmail.com>
2017-10-11 11:37:28 -0300

    Added voice over after OCR

--- Tesseract/ViewController.swift
+++ Tesseract/ViewController.swift
@@ -8,20 +8,53 @@
 
 import UIKit
 import TesseractOCR
+import AVFoundation
 
-class ViewController: UIViewController, G8TesseractDelegate {
+class ViewController: UIViewController, G8TesseractDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVSpeechSynthesizerDelegate  {
 
+    var tesseract = G8Tesseract(language: "eng")!
+    let picker = UIImagePickerController()
+    let synthesizer = AVSpeechSynthesizer()
+    
+    @IBOutlet var triggerCameraGesture: UITapGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let tesseract = G8Tesseract(language: "eng") else {return}
-        tesseract.delegate = self
-        tesseract.image = UIImage(named: "sample1.jpg")
-        tesseract.recognize()
-        print(tesseract.recognizedText)
+        self.tesseract.delegate = self
+        picker.delegate = self
+        picker.allowsEditing = false
+        picker.sourceType = .camera
+        picker.cameraDevice = .rear
+        triggerCameraGesture.numberOfTapsRequired = 2
+        triggerCameraGesture.numberOfTouchesRequired = 1
     }
 
+    @IBAction func triggerCameraDetected(_ sender: UITapGestureRecognizer) {
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    
+    
     func shouldCancelImageRecognition(for tesseract: G8Tesseract!) -> Bool {
         return false
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            self.tesseract.image = image
+            self.tesseract.recognize()
+            print(self.tesseract.recognizedText)
+            let utter = AVSpeechUtterance(string: self.tesseract.recognizedText)
+            self.synthesizer.speak(utter)
+        }
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        print("Finished uttering sentences.")
     }
 }
 
